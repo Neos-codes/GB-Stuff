333019d492c7e3702a78e448dceaa9628060b1a7
Neos <72319913+Neos-codes@users.noreply.github.com>
2020-11-02 19:55:08 -0300

    CompanionWalkingAnim() added

--- newPokemonRed/main.c
+++ newPokemonRed/main.c
@@ -13,17 +13,19 @@ void LoadData_Trainer();
 void LoadData_Companion();
 void LoadData_TestBKG();
 UINT8 Input();
-void scrollBKG(UINT8 x, UINT8 y, UINT8 dir);        // Se usa en TrainerMovement
+void scrollBKG(UINT8 x, UINT8 y, UINT8 dir, UINT8 companionFlag);        // Se usa en TrainerMovement
 void TrainerWalkingAnim(UINT8 dir);
 void CompanionWalkingAnim(UINT8 dir);
+void GetCompanionDir(UINT8 flag);
 
 GameObject trainer;
-GameObject pikachu;
+GameObject companion;
 UINT8 lastPosX, lastPosY;   // last trainer position (to calculate direction of companion)
-UINT8 animFrame = 0;        // Animation Frame of Trainer and Companion
+UINT8 trainerAnimFrame = 0; // Animation Frame of Trainer
 UINT8 isMove = 0;           // Boolean that indicates if Trainer is moving
 UINT8 dirX, dirY;           // direction of trainer movement
-UINT8 hasCompanion = 1, CompanionFlip = 0;
+UINT8 hasCompanion = 1, companionFlip = 0, CompanionDir, companionAnimFrame = 0;
+UINT8 CompanionLastDir = 2; // En la sandbox comienza hacia abajo
 
 
 void main(){
@@ -31,6 +33,7 @@ void main(){
     LoadData_Trainer();
     LoadData_Companion();
     LoadData_TestBKG();
+    //set_sprite_tile(39, 38);   // For Debug
 
     SHOW_SPRITES;
     SHOW_BKG;
@@ -67,6 +70,10 @@ void LoadData_Trainer(){
 }
 
 void LoadData_Companion(){
+
+    // Set Coordinates
+    companion.x = 9;    // This must variate in every map
+    companion.y = 7;
     // Load Data
     set_sprite_data(38, 24, PikachuSprites);
     // Sprites 38 - 49
@@ -76,10 +83,10 @@ void LoadData_Companion(){
     set_sprite_tile(6, 40);
     set_sprite_tile(7, 41);
     // Position of Sprites
-    move_sprite(4, 72, 56);
-    move_sprite(5, 80, 56);
-    move_sprite(6, 72, 64);
-    move_sprite(7, 80, 64);
+    move_sprite(4, 72, 60);   // 1 Tile de distancia con Trainer
+    move_sprite(5, 80, 60);
+    move_sprite(6, 72, 68);
+    move_sprite(7, 80, 68);
 }
 
 void LoadData_TestBKG(){
@@ -92,48 +99,53 @@ UINT8 Input(){
     if(joypad() & J_UP){
         if(testBKG[trainer.x + 20 * (trainer.y - 1)] != 0x03){   // OK
             isMove = 1;
-            //set_bkg_tiles(trainer.x, trainer.y - 2, 1, 1, 0);
+            lastPosY = trainer.y;
             trainer.y -= 2;
-            scrollBKG(0, -1, 1);
+            scrollBKG(0, -1, 1, 1);
         }
     }
     else if(joypad() & J_DOWN){
         if(testBKG[trainer.x + (20 * (trainer.y + 2))] != 0x03){   // Aqui está el error
             isMove = 1;
-            //set_bkg_tiles(trainer.x, trainer.y + 2, 1, 1, 0);
+            lastPosY = trainer.y;
             trainer.y += 2;
-            scrollBKG(0, 1, 2);
+            scrollBKG(0, 1, 2, 1);
         }
     }
     else if(joypad() & J_RIGHT){
         if(testBKG[trainer.x + 1 + 20 * trainer.y] != 0x03){
             isMove = 1;
-            //set_bkg_tiles(trainer.x + 1, trainer.y, 1, 1, 0);
+            lastPosX = trainer.x;
             trainer.x += 2;
-            scrollBKG(1, 0, 3);
+            scrollBKG(1, 0, 3, 0);
         }
     }
     else if(joypad() & J_LEFT){
         if(testBKG[trainer.x - 2 + (20 * trainer.y)] != 0x03){
             isMove = 1;
-            //set_bkg_tiles(trainer.x - 2, trainer.y, 1, 1, 0);
+            lastPosX = trainer.x;
             trainer.x -= 2;
-            scrollBKG(-1, 0, 4);
+            scrollBKG(-1, 0, 4, 0);
         }
     }
     else{
         return 0;
     }
-
     return isMove;
 }
 
-void scrollBKG(UINT8 x, UINT8 y, UINT8 dir){
+void scrollBKG(UINT8 x, UINT8 y, UINT8 dir, UINT8 companionFlag){
     UINT8 steps = 0;
-    UINT8 frame = 1;
+    GetCompanionDir(companionFlag);
     while(steps < 16){
-        if(steps == 0 || steps == 7){
-            TrainerWalkingAnim(dir);
+        if(steps == 0 || steps == 7){   // Para mezclar animaciones de trainer y companion
+            TrainerWalkingAnim(dir);    // separar cada step en un if
+            if(hasCompanion){
+                CompanionWalkingAnim(CompanionDir);
+            }
+        }
+        else if((steps == 3 || steps == 11) && hasCompanion){
+            CompanionWalkingAnim(CompanionDir);
         }
         scroll_bkg(x, y);
         steps++;
@@ -144,201 +156,288 @@ void scrollBKG(UINT8 x, UINT8 y, UINT8 dir){
 void TrainerWalkingAnim(UINT8 dir){
     
     if(dir == 1){   // Si va hacia arriba
-        if(animFrame == 0){             // Paso izquierdo
+        if(trainerAnimFrame == 0){             // Paso izquierdo
             set_sprite_tile(0, 26);
             set_sprite_tile(1, 27);
             set_sprite_tile(2, 28);
             set_sprite_tile(3, 29);
-            if(hasCompanion){
-                set_sprite_tile(4, 54);
-                set_sprite_tile(5, 55);
-                set_sprite_tile(6, 56);
-                set_sprite_tile(7, 57);
-                set_sprite_prop(4, 0x00);
-                set_sprite_prop(5, 0x00);
-                set_sprite_prop(6, 0x00);
-                set_sprite_prop(7, 0x00);
-                CompanionFlip = 0;
-            }
-            animFrame = 1;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(1);
+            }*/
+            trainerAnimFrame = 1;
         }
-        else if(animFrame == 2){         // Paso derecho
+        else if(trainerAnimFrame == 2){         // Paso derecho
             set_sprite_tile(0, 35);
             set_sprite_tile(1, 34);
             set_sprite_tile(2, 37);
             set_sprite_tile(3, 36);
-            if(hasCompanion){
-                set_sprite_tile(4, 55);
-                set_sprite_tile(5, 54);
-                set_sprite_tile(6, 57);
-                set_sprite_tile(7, 56);
-                set_sprite_prop(4, S_FLIPX);
-                set_sprite_prop(5, S_FLIPX);
-                set_sprite_prop(6, S_FLIPX);
-                set_sprite_prop(7, S_FLIPX);
-                CompanionFlip = 1;
-            }
-            animFrame++;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(1);
+            }*/
+            trainerAnimFrame++;
         }
-        else if(animFrame == 1 || animFrame == 3){  // Centro
+        else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
             set_sprite_tile(0, 30);
             set_sprite_tile(1, 31);
             set_sprite_tile(2, 32);
             set_sprite_tile(3, 33);
-            if(hasCompanion){
-                set_sprite_tile(4, 58);
-                set_sprite_tile(5, 59);
-                set_sprite_tile(6, 60);
-                set_sprite_tile(7, 61);
-                if(CompanionFlip){
-                    set_sprite_prop(4, 0x00);
-                    set_sprite_prop(5, 0x00);
-                    set_sprite_prop(6, 0x00);
-                    set_sprite_prop(7, 0x00);
-                    CompanionFlip = 0;
-                }
-            }
-            if(animFrame == 3)
-                animFrame = 0;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(1);
+            }*/
+            if(trainerAnimFrame == 3)
+                trainerAnimFrame = 0;
             else
-                animFrame = 2;
+                trainerAnimFrame = 2;
         }
     }
     if(dir == 2){   // Si va hacia abajo
-        if(animFrame == 0){             // Paso izquierdo
+        if(trainerAnimFrame == 0){             // Paso izquierdo
             set_sprite_tile(0, 4);
             set_sprite_tile(1, 5);
             set_sprite_tile(2, 6);
             set_sprite_tile(3, 7);
-            if(hasCompanion){
-                set_sprite_tile(4, 52);
-                set_sprite_tile(5, 53);
-                set_sprite_tile(6, 50);
-                set_sprite_tile(7, 51);
-                set_sprite_prop(4, 0x00);
-                set_sprite_prop(5, 0x00);
-                set_sprite_prop(6, 0x00);
-                set_sprite_prop(7, 0x00);
-                CompanionFlip = 0;
-            }
-            animFrame = 1;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(2);
+            }*/
+            trainerAnimFrame = 1;
         }
-        else if(animFrame == 2){         // Paso derecho
+        else if(trainerAnimFrame == 2){         // Paso derecho
             set_sprite_tile(0, 4);
             set_sprite_tile(1, 5);
             set_sprite_tile(2, 8);
             set_sprite_tile(3, 9);
-            if(hasCompanion){
-                set_sprite_tile(4, 53);
-                set_sprite_tile(5, 52);
-                set_sprite_tile(6, 51);
-                set_sprite_tile(7, 50);
-                set_sprite_prop(4, S_FLIPX);
-                set_sprite_prop(5, S_FLIPX);
-                set_sprite_prop(6, S_FLIPX);
-                set_sprite_prop(7, S_FLIPX);
-                CompanionFlip = 1;
-            }
-            animFrame++;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(2);
+            }*/
+            trainerAnimFrame++;
         }
-        else if(animFrame == 1 || animFrame == 3){  // Centro
+        else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
             set_sprite_tile(0, 0);
             set_sprite_tile(1, 1);
             set_sprite_tile(2, 2);
             set_sprite_tile(3, 3);
-            if(hasCompanion){
-                set_sprite_tile(4, 38);
-                set_sprite_tile(5, 39);
-                set_sprite_tile(6, 40);
-                set_sprite_tile(7, 41);
-                if(CompanionFlip){
-                    set_sprite_prop(4, 0x00);
-                    set_sprite_prop(5, 0x00);
-                    set_sprite_prop(6, 0x00);
-                    set_sprite_prop(7, 0x00);
-                    CompanionFlip = 0;
-                }
-            }
-            if(animFrame == 3)
-                animFrame = 0;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(2);
+            }*/
+            if(trainerAnimFrame == 3)
+                trainerAnimFrame = 0;
             else
-                animFrame = 2;
+                trainerAnimFrame = 2;
         }
     }
     if(dir == 3){    // Hacia la derecha
-        if(animFrame == 0 || animFrame == 2){  // Paso
+        if(trainerAnimFrame == 0 || trainerAnimFrame == 2){  // Paso
             set_sprite_tile(0, 19);
             set_sprite_tile(1, 18);
             set_sprite_tile(2, 21);
             set_sprite_tile(3, 20);
-            if(hasCompanion){
-                CompanionFlip = 1;   // Bandera que avisa inversion de Sprites
-                set_sprite_tile(4, 45);
-                set_sprite_tile(5, 44);
-                set_sprite_tile(6, 42);
-                set_sprite_tile(7, 43);
-                set_sprite_prop(4, S_FLIPX);
-                set_sprite_prop(5, S_FLIPX);
-                set_sprite_prop(6, S_FLIPX);
-                set_sprite_prop(7, S_FLIPX);
-            }
-            animFrame++;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(3);
+            }*/
+            trainerAnimFrame++;
         }
-        else if(animFrame == 1 || animFrame == 3){  // Centro
+        else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
             set_sprite_tile(0, 23);
             set_sprite_tile(1, 22);
             set_sprite_tile(2, 25);
             set_sprite_tile(3, 24);
-            if(hasCompanion){
-                set_sprite_tile(4, 49);
-                set_sprite_tile(5, 48);
-                set_sprite_tile(6, 47);
-                set_sprite_tile(7, 46);
-            }
-            if(animFrame == 3)
-                animFrame = 0;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(3);
+            }*/
+            if(trainerAnimFrame == 3)
+                trainerAnimFrame = 0;
             else
-                animFrame++;
+                trainerAnimFrame++;
             
         }
     }
     if(dir == 4){    // Hacia la izquierda
-        if(animFrame == 0 || animFrame == 2){  // Paso
+        if(trainerAnimFrame == 0 || trainerAnimFrame == 2){  // Paso
             set_sprite_tile(0, 10);
             set_sprite_tile(1, 11);
             set_sprite_tile(2, 12);
             set_sprite_tile(3, 13);
-            if(hasCompanion){
-                if(CompanionFlip){
-                    set_sprite_prop(4, 0x00);  // Quitar FlipX
-                    set_sprite_prop(5, 0x00);
-                    set_sprite_prop(6, 0x00);
-                    set_sprite_prop(7, 0x00);
-                    CompanionFlip = 0;
-                }
-                set_sprite_tile(4, 44);
-                set_sprite_tile(5, 45);
-                set_sprite_tile(6, 43);
-                set_sprite_tile(7, 42);
-            }
-            animFrame++;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(4);
+            }*/
+            trainerAnimFrame++;
         }
-        else if(animFrame == 1 || animFrame == 3){  // Centro
+        else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
             set_sprite_tile(0, 14);
             set_sprite_tile(1, 15);
             set_sprite_tile(2, 16);
             set_sprite_tile(3, 17);
-            if(hasCompanion){
-                set_sprite_tile(4, 48);
-                set_sprite_tile(5, 49);
-                set_sprite_tile(6, 46);
-                set_sprite_tile(7, 47);
-            }
-            if(animFrame == 3)
-                animFrame = 0;
+            /*if(hasCompanion){
+                CompanionWalkingAnim(4);
+            }*/
+            if(trainerAnimFrame == 3)
+                trainerAnimFrame = 0;
             else
-                animFrame++;
+                trainerAnimFrame++;
             
         }
+    }
+}
+
+void CompanionWalkingAnim(UINT8 dir){
+
+    // Hacia arriba
+    if(dir == 1){
+        if(companionAnimFrame == 0){             // Paso izquierdo
+            set_sprite_tile(4, 54);
+            set_sprite_tile(5, 55);
+            set_sprite_tile(6, 56);
+            set_sprite_tile(7, 57);
+            set_sprite_prop(4, 0x00);
+            set_sprite_prop(5, 0x00);
+            set_sprite_prop(6, 0x00);
+            set_sprite_prop(7, 0x00);
+            companionFlip = 0;
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 2){         // Paso derecho
+            set_sprite_tile(4, 55);
+            set_sprite_tile(5, 54);
+            set_sprite_tile(6, 57);
+            set_sprite_tile(7, 56);
+            set_sprite_prop(4, S_FLIPX);
+            set_sprite_prop(5, S_FLIPX);
+            set_sprite_prop(6, S_FLIPX);
+            set_sprite_prop(7, S_FLIPX);
+            companionFlip = 1;
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 1 || companionAnimFrame == 3){  // Centro
+            set_sprite_tile(4, 58);
+            set_sprite_tile(5, 59);
+            set_sprite_tile(6, 60);
+            set_sprite_tile(7, 61);
+            if(companionFlip){
+                set_sprite_prop(4, 0x00);
+                set_sprite_prop(5, 0x00);
+                set_sprite_prop(6, 0x00);
+                set_sprite_prop(7, 0x00);
+                companionFlip = 0;
+            }
+            if(companionAnimFrame == 3)
+                companionAnimFrame = 0;
+            else
+                companionAnimFrame++;
+        }
+    }
+    // Hacia Abajo
+    else if(dir == 2){
+        if(companionAnimFrame == 0){             // Paso izquierdo
+            set_sprite_tile(4, 52);
+            set_sprite_tile(5, 53);
+            set_sprite_tile(6, 50);
+            set_sprite_tile(7, 51);
+            set_sprite_prop(4, 0x00);
+            set_sprite_prop(5, 0x00);
+            set_sprite_prop(6, 0x00);
+            set_sprite_prop(7, 0x00);
+            companionFlip = 0;
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 2){         // Paso derecho
+            set_sprite_tile(4, 53);
+            set_sprite_tile(5, 52);
+            set_sprite_tile(6, 51);
+            set_sprite_tile(7, 50);
+            set_sprite_prop(4, S_FLIPX);
+            set_sprite_prop(5, S_FLIPX);
+            set_sprite_prop(6, S_FLIPX);
+            set_sprite_prop(7, S_FLIPX);
+            companionFlip = 1;
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 1 || companionAnimFrame == 3){  // Centro
+            set_sprite_tile(4, 38);
+            set_sprite_tile(5, 39);
+            set_sprite_tile(6, 40);
+            set_sprite_tile(7, 41);
+            if(companionFlip){
+                set_sprite_prop(4, 0x00);
+                set_sprite_prop(5, 0x00);
+                set_sprite_prop(6, 0x00);
+                set_sprite_prop(7, 0x00);
+                companionFlip = 0;
+            }
+            if(companionAnimFrame == 3)
+                companionAnimFrame = 0;
+            else
+                companionAnimFrame++;
+            
+        }
+    }
+    // Hacia la derecha
+    else if(dir == 3){
+        if(companionAnimFrame == 0 || companionAnimFrame == 2){  // Paso
+            companionFlip = 1;   // Bandera que avisa inversion de Sprites
+            set_sprite_tile(4, 45);
+            set_sprite_tile(5, 44);
+            set_sprite_tile(6, 42);
+            set_sprite_tile(7, 43);
+            set_sprite_prop(4, S_FLIPX);
+            set_sprite_prop(5, S_FLIPX);
+            set_sprite_prop(6, S_FLIPX);
+            set_sprite_prop(7, S_FLIPX);
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 1 || companionAnimFrame == 3){  // Centro
+            set_sprite_tile(4, 49);
+            set_sprite_tile(5, 48);
+            set_sprite_tile(6, 47);
+            set_sprite_tile(7, 46);
+            if(companionAnimFrame == 3)
+                companionAnimFrame = 0;
+            else
+                companionAnimFrame++;  
+        }
+    }
+    // Hacia la izquierda
+    else if(dir == 4){
+        if(companionAnimFrame == 0 || companionAnimFrame == 2){  // Paso
+            if(companionFlip){
+                set_sprite_prop(4, 0x00);  // Quitar FlipX
+                set_sprite_prop(5, 0x00);
+                set_sprite_prop(6, 0x00);
+                set_sprite_prop(7, 0x00);
+                companionFlip = 0;
+            }
+            set_sprite_tile(4, 44);
+            set_sprite_tile(5, 45);
+            set_sprite_tile(6, 43);
+            set_sprite_tile(7, 42);
+            companionAnimFrame++;
+        }
+        else if(companionAnimFrame == 1 || companionAnimFrame == 3){  // Centro
+            set_sprite_tile(4, 48);
+            set_sprite_tile(5, 49);
+            set_sprite_tile(6, 46);
+            set_sprite_tile(7, 47);
+            if(companionAnimFrame == 3)
+                companionAnimFrame = 0;
+            else
+                companionAnimFrame++;   
+        }
+    }
+}
+
+void GetCompanionDir(UINT8 flag){
+    char cDirX = (char)lastPosX - (char)trainer.x;
+    char cDirY = (char)lastPosY - (char)trainer.y;
+
+    if(cDirX < 0 && !flag){       // Se mueve hacia la derecha
+        CompanionDir = 3;
+    }
+    else if(cDirX > 0 && !flag){   // Se mueve hacia la izquierda
+        CompanionDir = 4;
+    }
+    else if(cDirY < 0 && flag){   // Se mueve hacia abajo
+        CompanionDir = 2;
+    }
+    else if(cDirY > 0 && flag){   // Se mueve hacia arriba
+        CompanionDir = 1;
     }
 }
