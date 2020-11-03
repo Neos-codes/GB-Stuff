62f539d7ef4bd09e129941185b9d14bd1225eac3
Neos <72319913+Neos-codes@users.noreply.github.com>
2020-11-03 16:54:29 -0300

    companion follow added

--- newPokemonRed/main.c
+++ newPokemonRed/main.c
@@ -16,16 +16,17 @@ UINT8 Input();
 void scrollBKG(UINT8 x, UINT8 y, UINT8 dir, UINT8 companionFlag);        // Se usa en TrainerMovement
 void TrainerWalkingAnim(UINT8 dir);
 void CompanionWalkingAnim(UINT8 dir);
-void GetCompanionDir(UINT8 flag);
+void GetCompanionDir(UINT8 flag);   // flag = 0 se mueve en X, flag 1 en eje Y , flag = companionFlag
+void CompanionMove(UINT8 flag);
 
 GameObject trainer;
 GameObject companion;
+UINT8 flag = 0;             // bit 0: companionMove() flag
 UINT8 lastPosX, lastPosY;   // last trainer position (to calculate direction of companion)
 UINT8 trainerAnimFrame = 0; // Animation Frame of Trainer
 UINT8 isMove = 0;           // Boolean that indicates if Trainer is moving
-UINT8 dirX, dirY;           // direction of trainer movement
+UINT8 trainerDir = 2, trainerLastDir = 2;   // direction of trainer movement
 UINT8 hasCompanion = 1, companionFlip = 0, CompanionDir, companionAnimFrame = 0;
-UINT8 CompanionLastDir = 2; // En la sandbox comienza hacia abajo
 
 
 void main(){
@@ -101,6 +102,8 @@ UINT8 Input(){
             isMove = 1;
             lastPosY = trainer.y;
             trainer.y -= 2;
+            trainerLastDir = trainerDir;
+            trainerDir = 1;
             scrollBKG(0, -1, 1, 1);
         }
     }
@@ -109,6 +112,8 @@ UINT8 Input(){
             isMove = 1;
             lastPosY = trainer.y;
             trainer.y += 2;
+            trainerLastDir = trainerDir;
+            trainerDir = 2;
             scrollBKG(0, 1, 2, 1);
         }
     }
@@ -117,6 +122,8 @@ UINT8 Input(){
             isMove = 1;
             lastPosX = trainer.x;
             trainer.x += 2;
+            trainerLastDir = trainerDir;
+            trainerDir = 3;
             scrollBKG(1, 0, 3, 0);
         }
     }
@@ -125,6 +132,8 @@ UINT8 Input(){
             isMove = 1;
             lastPosX = trainer.x;
             trainer.x -= 2;
+            trainerLastDir = trainerDir;
+            trainerDir = 4;
             scrollBKG(-1, 0, 4, 0);
         }
     }
@@ -136,8 +145,10 @@ UINT8 Input(){
 
 void scrollBKG(UINT8 x, UINT8 y, UINT8 dir, UINT8 companionFlag){
     UINT8 steps = 0;
+    //companionLastDir = CompanionDir;    // Se guarda la direccion anterior antes de obtener la nueva
     GetCompanionDir(companionFlag);
     while(steps < 16){
+        CompanionMove(!(steps%4));
         if(steps == 0 || steps == 7){   // Para mezclar animaciones de trainer y companion
             TrainerWalkingAnim(dir);    // separar cada step en un if
             if(hasCompanion){
@@ -151,6 +162,15 @@ void scrollBKG(UINT8 x, UINT8 y, UINT8 dir, UINT8 companionFlag){
         steps++;
         wait_vbl_done();
     }
+    /*if(flag){
+        steps = 0;
+        flag = 0;
+        while(steps < 4){
+            CompanionMove();
+            steps++;
+            wait_vbl_done();
+        }
+    }*/
 }
 
 void TrainerWalkingAnim(UINT8 dir){
@@ -161,9 +181,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 27);
             set_sprite_tile(2, 28);
             set_sprite_tile(3, 29);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(1);
-            }*/
             trainerAnimFrame = 1;
         }
         else if(trainerAnimFrame == 2){         // Paso derecho
@@ -171,9 +188,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 34);
             set_sprite_tile(2, 37);
             set_sprite_tile(3, 36);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(1);
-            }*/
             trainerAnimFrame++;
         }
         else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
@@ -181,9 +195,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 31);
             set_sprite_tile(2, 32);
             set_sprite_tile(3, 33);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(1);
-            }*/
             if(trainerAnimFrame == 3)
                 trainerAnimFrame = 0;
             else
@@ -196,9 +207,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 5);
             set_sprite_tile(2, 6);
             set_sprite_tile(3, 7);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(2);
-            }*/
             trainerAnimFrame = 1;
         }
         else if(trainerAnimFrame == 2){         // Paso derecho
@@ -206,9 +214,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 5);
             set_sprite_tile(2, 8);
             set_sprite_tile(3, 9);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(2);
-            }*/
             trainerAnimFrame++;
         }
         else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
@@ -216,9 +221,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 1);
             set_sprite_tile(2, 2);
             set_sprite_tile(3, 3);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(2);
-            }*/
             if(trainerAnimFrame == 3)
                 trainerAnimFrame = 0;
             else
@@ -231,9 +233,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 18);
             set_sprite_tile(2, 21);
             set_sprite_tile(3, 20);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(3);
-            }*/
             trainerAnimFrame++;
         }
         else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
@@ -241,9 +240,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 22);
             set_sprite_tile(2, 25);
             set_sprite_tile(3, 24);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(3);
-            }*/
             if(trainerAnimFrame == 3)
                 trainerAnimFrame = 0;
             else
@@ -257,9 +253,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 11);
             set_sprite_tile(2, 12);
             set_sprite_tile(3, 13);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(4);
-            }*/
             trainerAnimFrame++;
         }
         else if(trainerAnimFrame == 1 || trainerAnimFrame == 3){  // Centro
@@ -267,9 +260,6 @@ void TrainerWalkingAnim(UINT8 dir){
             set_sprite_tile(1, 15);
             set_sprite_tile(2, 16);
             set_sprite_tile(3, 17);
-            /*if(hasCompanion){
-                CompanionWalkingAnim(4);
-            }*/
             if(trainerAnimFrame == 3)
                 trainerAnimFrame = 0;
             else
@@ -439,5 +429,94 @@ void GetCompanionDir(UINT8 flag){
     }
     else if(cDirY > 0 && flag){   // Se mueve hacia arriba
         CompanionDir = 1;
+    }
+}
+
+void CompanionMove(UINT8 flag){
+    if(trainerLastDir != trainerDir){
+        if((trainerLastDir == 3 && trainerDir == 1) || (trainerLastDir == 2 && trainerDir == 4)){
+            scroll_sprite(4, 1, 1);
+            scroll_sprite(5, 1, 1);
+            scroll_sprite(6, 1, 1);
+            scroll_sprite(7, 1, 1);
+            if(flag){
+            scroll_sprite(4, 0, 1);
+            scroll_sprite(5, 0, 1);
+            scroll_sprite(6, 0, 1);
+            scroll_sprite(7, 0, 1);
+            }
+        }
+        else if((trainerLastDir == 3 && trainerDir == 2) || (trainerLastDir == 1 && trainerDir == 4)){   // TLD = 3       // De derecha hacia abajo
+            scroll_sprite(4, 1, -1);
+            scroll_sprite(5, 1, -1);
+            scroll_sprite(6, 1, -1);
+            scroll_sprite(7, 1, -1);
+            if(flag){
+            scroll_sprite(4, 0, -1);
+            scroll_sprite(5, 0, -1);
+            scroll_sprite(6, 0, -1);
+            scroll_sprite(7, 0, -1);
+            }
+        }
+        else if((trainerLastDir == 4 && trainerDir == 1) || (trainerLastDir == 2 && trainerDir == 3)){
+            scroll_sprite(4, -1, 1);
+            scroll_sprite(5, -1, 1);
+            scroll_sprite(6, -1, 1);
+            scroll_sprite(7, -1, 1);
+            if(flag){
+            scroll_sprite(4, 0, 1);
+            scroll_sprite(5, 0, 1);
+            scroll_sprite(6, 0, 1);
+            scroll_sprite(7, 0, 1);
+            }
+        }
+        else if((trainerLastDir == 4 && trainerDir == 2) || (trainerLastDir == 1 && trainerDir == 3)){
+            scroll_sprite(4, -1, -1);
+            scroll_sprite(5, -1, -1);
+            scroll_sprite(6, -1, -1);
+            scroll_sprite(7, -1, -1);
+            if(flag){
+            scroll_sprite(4, 0, -1);
+            scroll_sprite(5, 0, -1);
+            scroll_sprite(6, 0, -1);
+            scroll_sprite(7, 0, -1);
+            }
+        }
+        else if(trainerDir == 3 && trainerLastDir == 4){
+            scroll_sprite(4, -2, 0);
+            scroll_sprite(5, -2, 0);
+            scroll_sprite(6, -2, 0);
+            scroll_sprite(7, -2, 0);
+        }
+        else if(trainerDir == 4 && trainerLastDir == 3){
+            scroll_sprite(4, 2, 0);
+            scroll_sprite(5, 2, 0);
+            scroll_sprite(6, 2, 0);
+            scroll_sprite(7, 2, 0); 
+        }
+        else if(trainerDir == 2 && trainerLastDir == 1){
+            scroll_sprite(4, 0, -2);
+            scroll_sprite(5, 0, -2);
+            scroll_sprite(6, 0, -2);
+            scroll_sprite(7, 0, -2);
+            if(flag){
+                scroll_sprite(4, 0, -2);
+                scroll_sprite(5, 0, -2);
+                scroll_sprite(6, 0, -2);
+                scroll_sprite(7, 0, -2);  
+            }
+        }
+        else if(trainerDir == 1 && trainerLastDir == 2){
+            scroll_sprite(4, 0, 2);
+            scroll_sprite(5, 0, 2);
+            scroll_sprite(6, 0, 2);
+            scroll_sprite(7, 0, 2);
+            if(flag){
+                scroll_sprite(4, 0, 2);
+                scroll_sprite(5, 0, 2);
+                scroll_sprite(6, 0, 2);
+                scroll_sprite(7, 0, 2);
+            }
+        }
     }
 }
