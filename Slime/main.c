a94d637e1ac03482557260691031a2d19196fee9
Neos_codes <alexced96@outlook.es>
2021-07-01 11:26:39 -0400

    Slime map movement added

--- Slime/main.c
+++ Slime/main.c
@@ -9,19 +9,26 @@
 // ----- Functions ----- //
 //===========================
 // Slime
-// ------Animations
+// ------ Mechanics
+void Slime_move();
+// ------ Animations
 void Slime_idle_anim();
 
 // Utils
-//------VBlanks
+//------ VBlanks
 void vbl_update();
 
 //================================
 // ----- Global Variables ----- //
 //================================
+// Slime
+UINT8 state = 0;       // For animations (0 - 1)
+UINT8 slime_dir = 0;   // 0 up, 1 down, 2 right, 3 left
+UINT8 frames_anim = 0; // Use for animations (example, 20 frames = 1 state of sprite in idle_slime)
 
+// Utils
+//----- VBlanks
 UINT8 vbl_count = 0;
-UINT8 state = 0;
 
 void main(){
 
@@ -49,21 +56,51 @@ void main(){
         if(!vbl_count)
             wait_vbl_done();
         vbl_count = 0;
-
         Slime_idle_anim();
+        slime_dir = joypad();
+        Slime_move();
 
     }
 
 }
 
+void Slime_move(){
+    if(slime_dir & J_RIGHT){
+        scroll_sprite(0, 1, 0);
+        scroll_sprite(1, 1, 0);
+        scroll_sprite(2, 1, 0);
+        scroll_sprite(3, 1, 0);
+    }
+    else if(slime_dir & J_LEFT){
+        scroll_sprite(0, -1, 0);
+        scroll_sprite(1, -1, 0);
+        scroll_sprite(2, -1, 0);
+        scroll_sprite(3, -1, 0);
+    }
+    else if(slime_dir & J_UP){
+        scroll_sprite(0, 0, -1);
+        scroll_sprite(1, 0, -1);
+        scroll_sprite(2, 0, -1);
+        scroll_sprite(3, 0, -1);
+    }
+    else if(slime_dir & J_DOWN){
+        scroll_sprite(0, 0, 1);
+        scroll_sprite(1, 0, 1);
+        scroll_sprite(2, 0, 1);
+        scroll_sprite(3, 0, 1);
+    }
+}
+    
+
 void Slime_idle_anim(){
-    if(state){
-            set_sprite_tile(0, 5);
-            set_sprite_tile(1, 6);
-            set_sprite_tile(2, 7);
-            set_sprite_tile(3, 8);
-            state = !state;
-            delay(1000);
+    frames_anim++;
+    if(frames_anim == 30){
+        if(state){
+                set_sprite_tile(0, 5);
+                set_sprite_tile(1, 6);
+                set_sprite_tile(2, 7);
+                set_sprite_tile(3, 8);
+                state = !state;
         }
         else{
             set_sprite_tile(0, 0);
@@ -71,8 +108,9 @@ void Slime_idle_anim(){
             set_sprite_tile(2, 2);
             set_sprite_tile(3, 3);
             state = !state;
-            delay(1000);
         }
+        frames_anim = 0;
+    }
 }
 
 void vbl_update(){
